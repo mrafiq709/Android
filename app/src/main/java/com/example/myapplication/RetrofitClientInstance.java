5f2930dd0080310ea47c2079936c0ae07fba856a
Rafiq <mrafiq709@gmail.com>
2019-08-06 16:09:47 +0700

    Update RetrofitClientInstance.java

--- app/src/main/java/com/example/myapplication/RetrofitClientInstance.java
+++ app/src/main/java/com/example/myapplication/RetrofitClientInstance.java
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class RetrofitClientInstance {
 
     private static Retrofit retrofit;
-    private static final String BASE_URL = "http://192.168.100.62:8080/";
+    private static final String BASE_URL = "your_base_url";
 
     public static Retrofit getRetrofitInstance() {
         retrofit = new retrofit2.Retrofit.Builder()
